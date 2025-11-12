624a9154a9ce1e2a5ccda2d05712a0c41ac4953d
IvanRoussev <ivan.roussev12@gmail.com>
2025-11-12 14:37:13 +0200

    removing frontend and go backend for now

--- attack-cluster/internal/db/models.go
+++
@@ -1,9 +0,0 @@
-package db
-
-type Player struct {
-	ID         uint   `gorm:"primaryKey"`
-	Name       string `gorm:"unique;not null"`
-	NumAttacks int    `gorm:"default:0"`
-}
-
-
--- frontend/app.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-
-const button = document.getElementById('incrementBtn');
-const counter = document.getElementById('counter');
-
-button.addEventListener('click', () => {
-  count++;
-  counter.textContent = count;
-});
